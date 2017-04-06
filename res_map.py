1633fd677578bb44bb8c15fc7a8c4ef44d6c5fa4
GauravWaghmare <gauravwaghmare30@gmail.com>
2017-04-07 04:21:12 +0530

    Added response map

---
+++ res_map.py
@@ -0,0 +1,162 @@
+res = []
+temp = ("HIV (Human Immunodeficiency Virus) is a virus. You may hear that someone is "
+		"HIV infected, has HIV infection, or has HIV disease. These are all terms that mean the "
+		"person has HIV in his or her body and can pass the virus to other people. "
+		"HIV attacks the body's immune system. The immune system protects the body "
+		"from infections and disease, but has no clear way to protect it from HIV. Without "
+		"treatment, most people infected with HIV become less able to fight off the germs "
+		"that we are exposed to every day. Many of these germs do not usually make a "
+		"healthy person sick, but they can cause life-threatening infections and cancers in "
+		"a person whose immune system has been weakened by HIV. HIV treatments can "
+		"slow this process and allow people with HIV to live longer, healthier lives (see "
+		"question 66). \n"
+		"People infected with HIV may have no symptoms for ten or more years. They may "
+		"not know they are infected. An HIV test is the only way to find out if you have HIV")
+res.append(temp)
+temp = ("AIDS (Acquired Immune Deficiency Syndrome) is a late stage of HIV disease. "
+		"According to the Centers for Disease Control and Prevention (CDC), a person "
+		"with HIV infection has AIDS when he or she: \n"
+		"-> has a CD4 cell count (a way to measure the strength of the immune system) "
+		"that falls below 200. A normal CD4 cell count is 500 or higher. \n"
+		"OR \n"
+		"-> develops any of the specific, serious conditions - also called AIDS-defining "
+		"illnesses - that are linked with HIV infection (see Appendix for a list of these "
+		"conditions). ")
+res.append(temp)
+temp = ("A person of any age, sex, race, ethnic group, religion, economic background, or "
+		"sexual orientation can get HIV. "
+		"Those who are most at risk are: \n"
+		"-> people who have 'unprotected sex' with someone "
+		"who has HIV. Unprotected sex means vaginal, anal, "
+		"or oral sex without using a condom. \n"
+		"-> people who share needles, syringes, or other "
+		"equipment to inject drugs, steroids, or even vitamins "
+		"or medicine with someone who has HIV. \n"
+		"-> Babies can potentially become infected during their "
+		"mothers' pregnancy, during delivery, or after birth "
+		"in the immediate post-partum period. They can also "
+		"become infected through breastfeeding. \n"
+		"A person of any "
+		"age, sex, race, "
+		"ethnic group, "
+		"religion, economic "
+		"background, or "
+		"sexual orientation "
+		"can get HIV. \n"
+		"-> Health care and maintenance workers who may be exposed to blood and/or "
+		"body fluids at work sometimes get infected through on-the-job exposures like "
+		"needle-stick injuries (see question 31). ")
+res.append(temp)
+temp = ("Medicines that fight HIV have helped many people with HIV and AIDS live years "
+		"and even decades longer than was possible in the past, before effective treatment "
+		"was available. HIV treatments are not a cure, and they do not work equally well for "
+		"everyone, but they have extended the lives of many people with HIV and AIDS. "
+		"Without treatment, some people live for just a few years after getting HIV. Others "
+		"live much longer. Researchers are studying a small number of people with HIV "
+		"who have not become ill for more than ten years, even without any HIV treatment. "
+		"However, these people are still infected with HIV and can pass the virus to others. ")
+res.append(temp)
+temp = ("No. There is no vaccine to prevent HIV infection. Researchers are working to "
+		"develop a vaccine. Vaccines in development are being tested to find out if they "
+		"work. ")
+res.append(temp)
+temp = ("No. There is no cure for HIV or AIDS. However, there are medicines that fight "
+		"HIV and help people with HIV and AIDS live longer, healthier lives. ")
+res.append(temp)
+temp = (" According to the United Nations organization UNAIDS, as of 2003 there were "
+		"an estimated 40 million persons living with HIV and AIDS worldwide. Of these, "
+		"37 million were adults, and 2.5 million were under age 15. The overwhelming "
+		"majority of persons with HIV live in resource-poor countries. "
+		"As of December 2002, 517,000 persons were known to be living with HIV and AIDS "
+		"in the United States. The U.S. Centers for Disease Control and Prevention (CDC) "
+		"estimates that 170,000 more Americans are infected with HIV but do not know it. "
+		"Additionally, CDC estimates that 501,669 persons had died from AIDS in the U.S. "
+		"as of December 2002. ")
+res.append(temp)
+temp = (" As of December 2007, more than 180,674 persons in New York State had been "
+		"diagnosed with AIDS; approximately 73,889 of those persons are still living. Of "
+		"those 73,889 persons living with AIDS: \n"
+		"-> 44% are African American. \n"
+		"-> 30% are Hispanic. \n"
+		"-> 25% are white. \n"
+		"-> 0.7% are Asian/Pacific Islander. \n"
+		"-> 0.1% are Native American. \n"
+		"-> 26% are women. \n"
+		"-> 5% are under the age of 25. \n"
+		"-> 15% are over the age of 50.")
+res.append(temp)
+temp = ("Update not available")
+res.append(temp)
+temp = (" HIV is spread when infected blood, semen, vaginal fluids, "
+		"or breast milk gets into the bloodstream of another person "
+		"through: "
+		"-> direct entry into a blood vessel; "
+		"-> mucous linings, such as the vagina, rectum, penis, "
+		"mouth, eyes, or nose; or \n"
+		"-> a break in the skin. "
+		"HIV is not spread through saliva (spit). "
+		"You can only get "
+		"HIV if infected "
+		"blood, semen, "
+		"vaginal fluids, or "
+		"breast milk gets "
+		"into your body. \n"
+		"HIV is spread in the following ways: \n"
+		"-> Having vaginal, anal, or oral sex without using a condom. \n"
+		"-> Sharing needles, syringes, or works to inject drugs, vitamins, hormones, "
+		"steroids, or medicines. \n"
+		"-> Women with HIV infection can pass HIV to their babies during pregnancy, "
+		"delivery, and breastfeeding. \n"
+		"-> People who are exposed to blood and/or body fluids at work, like health care "
+ 		"workers, may be exposed to HIV through needle-sticks or other on-the-job "
+		"exposures. ")
+res.append(temp)
+temp = ("No. But it is important to know that you can be infected by a single exposure to "
+		"HIV-infected blood, semen, or vaginal fluids. Whether a person becomes infected "
+		"after being exposed to HIV depends on how the virus enters the body and the "
+		"amount of virus that enters the body")
+res.append(temp)
+temp = ("To be updated")
+res.append(temp)
+temp = ("To be updated")
+res.append(temp)
+temp = ("To be updated")
+res.append(temp)
+temp = ("To be updated")
+res.append(temp)
+temp = ("To be updated")
+res.append(temp)
+temp = ("To be updated")
+res.append(temp)
+temp = ("To be updated")
+res.append(temp)
+temp = ("To be updated")
+res.append(temp)
+temp = ("To be updated")
+res.append(temp)
+temp = ("To be updated")
+res.append(temp)
+temp = ("To be updated")
+res.append(temp)
+temp = ("To be updated")
+res.append(temp)
+temp = ("To be updated")
+res.append(temp)
+temp = ("To be updated")
+res.append(temp)
+temp = ("To be updated")
+res.append(temp)
+temp = ("To be updated")
+res.append(temp)
+temp = ("To be updated")
+res.append(temp)
+temp = ("To be updated")
+res.append(temp)
+temp = ("To be updated")
+res.append(temp)
+temp = ("To be updated")
+res.append(temp)
+temp = ("To be updated")
+res.append(temp)
+def responses():
+	return res
