22b4a360f2d8b7f85b0cc2a7d38bef1002ff253c
GauravWaghmare <gauravwaghmare30@gmail.com>
2017-04-10 02:42:00 +0530

    Interface

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import csv
 
 from nltk.stem import *
 import nltk
+from nltk.stem.porter import *
 
 import numpy as np
 import scipy
@@ -12,6 +13,8 @@ from sklearn.naive_bayes import MultinomialNB
 
 from res_map import responses
 
+file_path = "/home/gaurav/Github/Naive_Bayes_Sentence_Classifier/data.csv"
+
 class Proto(object):
 
 	def __init__(self):
@@ -19,15 +22,16 @@ class Proto(object):
 		self.no_words = 0
 		self.clf = MultinomialNB()
 		self.res = responses()
-		self.vocab = dict()
+		self.stems = []
+		self.word_pos = dict()
 
 	# Read the CSV file into a list of lists
-	def read_data(self, file_path):
+	def read_data(self):
 		data = []
 		with open(file_path) as csvfile:
-	    	spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-	    	for row in spamreader:
-	        	data.append(row)
+			spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+			for row in spamreader:
+				data.append(row)
 		# Remove the first row
 		data = data[1:]
 		self.no_ques = len(data)
@@ -35,83 +39,100 @@ class Proto(object):
 
 	# Change all characters to lower case
 	def lower_case(self):
-		data = read_data()
-		for element in self.data:
-	    	element[0] = element[0].lower()
-	    return data
+		data = self.read_data()
+		for element in data:
+			element[0] = element[0].lower()
+		return data
 
 	# Change all sentences to tokens
 	def tokenize(self):
-		data = lower_case()
+		data = self.lower_case()
 		sentences = []
 		for element in data:
-	    	sentences.append(nltk.word_tokenize(element[0]))
-	    return sentences
+			sentences.append(nltk.word_tokenize(element[0]))
+		return sentences
 
 	# Change all tokens to their respective stems
-	def stemify():
-		stems = []
-		sentences = tokenize()
-		from nltk.stem.porter import *
+	def stemify(self):
+		sentences = self.tokenize()
+		print("Sentences ", len(sentences))
 		stemmer = PorterStemmer()
 		for sentence in sentences:
-	    	stem = [stemmer.stem(word) for word in sentence]
-	    	stems.append(stem)
-	    return stems
+			stem = [stemmer.stem(word) for word in sentence]
+	    	self.stems.append(stem)
 
 	# Construct vocabulary
 	def build_voc(self):
-		sentences = stemify()
+		vocab = dict()
+		sentences = self.stemify()
 		vocab_size = 0
-		for i in self.sentences:
-	    	for ii in i:
-	        	vocab[ii] = 1
-		vocab_size = len(self.vocab)
+		for i in self.stems:
+			for ii in i:
+				vocab[ii] = 1
+		vocab_size = len(vocab)
 		self.no_words = vocab_size
-		self.X = np.zeros((len(data), vocab_size))
 		return vocab
 
 	# Construct a map to store position of each word in the vocabulary
-	def word_index(vocab):
+	def word_index(self):
+		vocab = self.build_voc()
 		vocab_arr = list(vocab.keys())
 		vocab_arr.sort()
-		word_pos = dict()
 		i = 0
 		for word in vocab_arr:
-	    	word_pos[word] = i
+			self.word_pos[word] = i
 	    	i += 1
-	    return word_pos
 
-	def populateX(stems):
+	def populateX(self):
+		X = np.zeros((self.no_ques, self.no_words))
+		print(len(self.stems))
+		print(self.no_ques)
+		for i in range(self.no_ques):
+			for word in self.stems[i]:
+				X[i, self.word_pos[word]] += 1
+		return X
+
+	def populateY(self):
+		Y = np.zeros((self.no_ques))
+		data = read_data(file_path)
 		for i in range(no_ques):
-	    	for word in stems[i]:
-	        	self.X[i, word_pos[word]] += 1
+			Y[i] = data[i][1]
+		return Y
 
-	def populateY(data):
-		for i in range(no_ques):
-	    	self.Y[i] = data[i][1]
+	def train(self):
+		self.word_index()
+		X = self.populateX()
+		Y = self.populateY()
+		self.clf.fit(X, Y)
 
-	def train():
-		self.clf.fit(self.X, self.Y)
-
-	def vector(string):
+	def vector(self, string):
 	    string = string.lower()
 	    tokens = nltk.word_tokenize(string)
 	    stems = [stemmer.stem(word) for word in tokens]
-	    vec = np.zeros((1, no_words))
+	    vec = np.zeros((1, self.no_words))
 	    for stem in stems:
-	        vec[0][word_pos[stem]] += 1
+	        vec[0][self.word_pos[stem]] += 1
 	    return vec
 
-	def respond():
+	def respond(self):
 	    ask = raw_input()
 	    vec = vector(ask)
-	    no = clf.predict(vec)[0]
+	    no = self.clf.predict(vec)[0]
 	    no = int(no)
 	    print (res[no])
 
-	def __main__():
-		respond()
+def __main__():
+	proto = Proto()
+	proto.train()
+	ans = "Yes"
+	while ans=="yes":
+		print("Please input your query")
+		proto.respond()
+		print()
+		print("Do you have any more queries?")
+		ans = raw_input()
+
+__main__()
 
 
 
